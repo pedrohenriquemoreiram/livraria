9238b9f04f884d7bbf0e006ccd3e949b9f182238
Pedro Henrique <contatopedro.henrique.moreira.m@gmail.com>
2024-12-01 11:20:42 -0300

    Inclusão da foto de perfil no usuário

--- core/admin.py
+++ core/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ["email", "name"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_("Personal Info"), {"fields": ("name", "passage_id")}),
+        (_("Personal Info"), {"fields": ("name", "passage_id", "foto")}),
         (
             _("Permissions"),
             {
