6d5574aff8ee0bf16218767176ff2a3aeebebb94
Pedro Henrique <contatopedro.henrique.moreira.m@gmail.com>
2024-12-04 17:48:54 -0300

    Criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
 from .livro import LivroSerializer, LivroRetrieveSerializer, LivroListSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, CompraCreateUpdateSerializer, ItensCompraSerializer
