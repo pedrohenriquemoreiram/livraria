6a988f31bbf1d45e08bcbc40ed2a0cdef85735b2
Pedro Henrique <contatopedro.henrique.moreira.m@gmail.com>
2024-12-18 15:59:52 -0300

    Acrescentando a data da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField, CurrentUserDefault, HiddenField, ValidationError
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField, CurrentUserDefault, HiddenField, ValidationError, DateTimeField
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
@@ -18,13 +18,14 @@ class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
     total = SerializerMethodField()
+    data = DateTimeField(read_only=True)
 
     def get_total(self, instance):
         return sum(item.livro.preco * item.quantidade for item in instance.itens.all())
 
     class Meta:
         model = Compra
-        fields = ("id", "usuario", "status", "total", "itens")
+        fields = ("id", "usuario", "status", "total", "data", "itens")
 
 
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
